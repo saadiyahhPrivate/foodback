fc122ea7d1a5a6a31243e04543d044cad92cbd3b
Abdi-Hakin Dirie <abdihd@mit.edu>
2014-10-18 15:45:52 -0400

    edit

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
+var config = require('./data/config');
+var models = require('./data/models');
 
 var app = express();
 
