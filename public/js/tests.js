1248fc71a086adf4baa4a058f4407068a3ecc3f2
Abdi-Hakin Dirie <abdihd@mit.edu>
2014-10-25 18:43:54 -0400

    fixed a bug with search

--- public/js/tests.js
+++ public/js/tests.js
@@ -24,8 +24,11 @@ function error(xhr) {
 
 // Assert that the given review is equivalent to the test review.
 function checkReview(assert, newReview) {
+<<<<<<< HEAD
 //    assert.strictEqual(newReview.author, user.username, 'Check author.');
     assert.strictEqual(newReview.author.name, user.name);
+=======
+>>>>>>> fcbe195647952d597978351d266e4e31e00ed0e7
     assert.strictEqual(newReview.scope.hall, review.hall, 'Check hall.');
     assert.strictEqual(newReview.scope.period, review.period, 'Check period.');
     assert.equal(newReview.rating, review.rating, 'Check rating.');
