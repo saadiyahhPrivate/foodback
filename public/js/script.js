2c47bf470a8da27da38eb6a6d064b836b5a70196
Abdi-Hakin Dirie <abdihd@mit.edu>
2014-10-26 21:57:04 -0400

    added skeleton for home page

---
+++ public/js/script.js
@@ -0,0 +1,25 @@
+function toggleReviewForm() {
+    var form = $('#post_form');
+    var inView = form.data('inView');
+    if (inView) {
+        form.css('max-height','0');
+        form.css('opacity','0');
+    } else {
+        form.css('max-height','40em');
+        form.css('opacity','1');
+    }
+    inView = !inView;
+    var inView = form.data('inView', inView);
+}
+
+function postReview() {
+    // Saadiyah
+
+    return false; // to avoid reloading
+}
+
+$(function () {
+    $('#post_form').data('inView', false);
+    $('#make_button').click(toggleReviewForm);
+    $('#post_button').click(postReview);
+});
