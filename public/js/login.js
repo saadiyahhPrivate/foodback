7e05d236054abc37ed5975490978664c6f8c5606
Sophia Wang <skw@mit.edu>
2014-10-27 15:24:18 -0400

    JS fixes

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,29 @@
-$('#login-username').popover({
-	trigger: 'focus',
-	container: 'body',
-	placement: 'left',
-	content: 'Your username is the same as your MIT Kerberos ID.'
-});
+$(document).ready(function() {
+	$('#login-username').popover({
+		trigger: 'focus',
+		container: 'body',
+		placement: 'left',
+		content: 'Your username is the same as your MIT Kerberos ID.'
+	});
 
-$('#signup-kerberos').popover({
-	trigger: 'focus',
-	container: 'body',
-	placement: 'right',
-	content: 'You must use your @mit.edu email address to sign up for a Foodback MIT account.'
-});
+	$('#signup-kerberos').popover({
+		trigger: 'focus',
+		container: 'body',
+		placement: 'right',
+		content: 'You must use your @mit.edu email address to sign up for a Foodback MIT account.'
+	});
 
-$('#signup-name').popover({
-	trigger: 'focus',
-	container: 'body',
-	placement: 'right',
-	content: 'Your first name will be displayed with any reviews that you post. 20 character limit.'
-});
+	$('#signup-name').popover({
+		trigger: 'focus',
+		container: 'body',
+		placement: 'right',
+		content: 'Your first name will be displayed with any reviews that you post. 20 character limit.'
+	});
 
-$('#signup-password').popover({
-	trigger: 'focus',
-	container: 'body',
-	placement: 'right',
-	content: 'For your security, please do NOT use your MIT Kerberos password. 30 character limit.'
+	$('#signup-password').popover({
+		trigger: 'focus',
+		container: 'body',
+		placement: 'right',
+		content: 'For your security, please do NOT use your MIT Kerberos password. 30 character limit.'
+	});
 });
