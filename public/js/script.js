05cecf7034c632bffd65048652910c3706ab4ec7
Sophia Wang <skw@mit.edu>
2014-10-28 00:06:17 -0400

    More logout updates

--- public/js/script.js
+++ public/js/script.js
@@ -17,6 +17,7 @@ function logout() {
 		success: function(response) {
 			$('#user-header').text("");
 			$('#user-header').append('<li><a href="/users/login">Log In or Sign Up</a></li>');
+			$('#post_form').data('inView', false);
 			$('make_button').hide();
 		},
 		error: error
